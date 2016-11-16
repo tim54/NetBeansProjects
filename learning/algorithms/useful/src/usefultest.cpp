8a9b6198a583bca326d718666e40576916896265
tim54 <tim54@rambler.ru>
2016-11-16 23:25:56 +0300

    added some binary func

--- learning/algorithms/useful/src/usefultest.cpp
+++ learning/algorithms/useful/src/usefultest.cpp
@@ -6,14 +6,50 @@
 using namespace std;
 
 int bitsCount(int num){
-    int size = sizeof(int);
-    
+    int counter = 0;
+    while (num != 0){
+        if ((num & 1) != 0)
+            counter++;
+        num >>= 1;
+    }
+    return counter;
 }
 
-int bitSet(int num, int n){
+int fourBitsReverse(char num){
+    // cout << "Sizeof char: " << sizeof(char) << endl;
+    int counter = 0;
+    bool buf[8];
+    for (int i = 0; i < 8; i++){
+        buf[7 - i] = num & 1;
+        num >>= 1;
+        cout << "Buf 1: i: " << i << ", val: " << buf[7 - i] << endl;
+    }
+    
+    bool tmp;
+    for (int i = 0; i < 4; i++){
+        tmp = buf[i];
+        buf[i] = buf[7 - i];
+        buf[7 - i] = tmp;
+        cout << "Buf 2: " << buf[7 - i] << endl;
+    }
+    
+    return 0;
+}
+
+int bitSet(int num, int n) {
     
     int mask = 1 << (n - 1);
     return num | mask;
+}
+
+bool bitGet(int num, int n) {
+    int mask = 1 << (n - 1);
+    return num & mask;
+}
+
+int clearBit(int num, int n){
+    int mask = ~(1 << (n - 1));
+    return num & mask;
 }
 
 /*
@@ -27,13 +63,18 @@ int main(int argc, char** argv) {
     // =======================================================================
     // Binary operations test
     // =======================================================================
-    int num = 4;
+    int num = 255;
     cout << bitset<8>(num) << endl;
     
     int modifNum = bitSet(num, 5);
     cout << bitset<8>(modifNum) << endl;
     
+    int res = bitsCount(num);
+    cout << "Res: " << res << endl;
     
+    cout << "A: " << bitset<8>('A') << endl;
+    res = fourBitsReverse('A');
+    cout << "res: " << bitset<32>(res) << endl;
     return 0;
 }
 
