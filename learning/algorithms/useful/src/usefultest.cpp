aca5420133b86c8463560354d8facadc0608a4c2
Kovalevsky Artem-RCFT46 <Artem.Kovalevsky@motorolasolutions.com>
2016-11-16 19:09:14 +0300

    changes in bytes operations
    Change-Id: Ia1ae2bf8963c358174fedd64216833f7fd2f95b8

--- learning/algorithms/useful/src/usefultest.cpp
+++ learning/algorithms/useful/src/usefultest.cpp
@@ -1,11 +1,18 @@
 #include "Date.h"
+#include <iostream>
 #include <cstdlib>
+#include <bitset>
 
 using namespace std;
 
+int bitsCount(int num){
+    int size = sizeof(int);
+    
+}
+
 int bitSet(int num, int n){
     
-    int mask = 1 << (n + 1);
+    int mask = 1 << (n - 1);
     return num | mask;
 }
 
@@ -15,11 +22,17 @@ int bitSet(int num, int n){
 int main(int argc, char** argv) {
     
     Date date = Date(21, Date::Month(06), 1985);
-    // date.add_day(1);
+
     printf("Date is %s\n", date.string_rep().c_str());
-    
+    // =======================================================================
+    // Binary operations test
+    // =======================================================================
     int num = 4;
-    printf("Num is: %d\n", num);
+    cout << bitset<8>(num) << endl;
+    
+    int modifNum = bitSet(num, 5);
+    cout << bitset<8>(modifNum) << endl;
+    
     
     return 0;
 }
